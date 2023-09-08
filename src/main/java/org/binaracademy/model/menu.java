7beed88ad1078e01a821a5796292122a848a2327
Nur Ainun <ainun9874@gmail.com>
2023-09-08 17:44:31 +0700

    first commit

---
+++ src/main/java/org/binaracademy/model/menu.java
@@ -0,0 +1,15 @@
+package org.binaracademy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public class menu {
+    // berisikan spesifikasi data, biasanya pojo class
+
+    private String namaMakanan;
+    private double hargaMakanan;
+
+}
